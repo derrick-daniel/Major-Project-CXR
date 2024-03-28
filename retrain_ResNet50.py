c7fd6b2ba3e8e3e17ac1510cc068e3f9985c1f97
derrick-daniel <=99546535+derrick-daniel@users.noreply.github.com>
2024-03-28 09:19:04 +0530

    Changes: Image path in  and resnet50 weights in model_ResNet50.py

--- retrain_ResNet50.py
+++ retrain_ResNet50.py
@@ -5,7 +5,7 @@ import model_ResNet50 as M
 
 # you will need to customize PATH_TO_IMAGES to where you have uncompressed
 # NIH images
-PATH_TO_IMAGES = "/kaggle/input/nih-chest-xrays-224-gray/images"
+PATH_TO_IMAGES = "/kaggle/input/images"
 WEIGHT_DECAY = 1e-4
 LEARNING_RATE = 0.01
 preds, aucs = M.train_cnn(PATH_TO_IMAGES, LEARNING_RATE, WEIGHT_DECAY)
