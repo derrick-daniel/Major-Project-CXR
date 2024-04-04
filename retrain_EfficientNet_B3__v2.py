890dbb5ada79f557e61e3d34025cc5008480fc37
derrick-daniel <=99546535+derrick-daniel@users.noreply.github.com>
2024-04-04 20:45:27 +0530

    Added remaining three v2 models

---
+++ retrain_EfficientNet_B3__v2.py
@@ -0,0 +1,25 @@
+import cxr_dataset as CXR
+import eval_model as E
+import model_EfficientNet_B3__v2 as M
+import os
+
+# Define your potential paths
+PATH_TO_IMAGES_OPTIONS = [
+    "/kaggle/input/nih-chest-xrays-224-gray/images",
+    "/kaggle/input/images"
+]
+
+# Function to determine the valid path
+def find_valid_path(paths_list):
+    for path in paths_list:
+        if os.path.exists(path):
+            print(f"Using path: {path}")
+            return path
+    raise FileNotFoundError("None of the paths exist.")
+
+# Try to find a valid path
+PATH_TO_IMAGES = find_valid_path(PATH_TO_IMAGES_OPTIONS)
+WEIGHT_DECAY = 1e-4
+LEARNING_RATE = 0.01
+preds, aucs = M.train_cnn(PATH_TO_IMAGES, LEARNING_RATE, WEIGHT_DECAY)
+
